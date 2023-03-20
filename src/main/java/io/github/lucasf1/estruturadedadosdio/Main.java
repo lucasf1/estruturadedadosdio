a604402df33c9ff3d8d898ffffc9fbee163caf7b
Lucas Farias <lucasf1@gmail.com>
2023-03-20 17:45:32 -0300

    Criação da árvore binária.

--- src/main/java/io/github/lucasf1/estruturadedadosdio/Main.java
+++ src/main/java/io/github/lucasf1/estruturadedadosdio/Main.java
@@ -1,5 +1,7 @@
 package io.github.lucasf1.estruturadedadosdio;
 
+import io.github.lucasf1.estruturadedadosdio.model.Obj;
+
 public class Main {
     private static void testarPilha() {
         Pilha<Integer> minhaPilha = new Pilha<>();
@@ -101,11 +103,34 @@ public class Main {
         }
     }
 
+    private static void testarArvoreBinaria() {
+
+        ArvoreBinaria<Obj> minhaArvore = new ArvoreBinaria<Obj>();
+        minhaArvore.inserir(new Obj(13));
+        minhaArvore.inserir(new Obj(10));
+        minhaArvore.inserir(new Obj(25));
+        minhaArvore.inserir(new Obj(2));
+        minhaArvore.inserir(new Obj(12));
+        minhaArvore.inserir(new Obj(20));
+        minhaArvore.inserir(new Obj(31));
+        minhaArvore.inserir(new Obj(29));
+        minhaArvore.inserir(new Obj(32));
+
+        minhaArvore.exibirInOrdem();
+        minhaArvore.exibirPreOrdem();
+        minhaArvore.exibirPosOrdem();
+
+
+    }
+
     public static void main(String[] args) {
         // testarPilha();
         // testarFila();
         // testarListaEncadeada();
         // testarListaDuplamenteEncadeada();
-        testarListaCircular();
+        // testarListaCircular();
+        testarArvoreBinaria();
     }
+
+
 }
